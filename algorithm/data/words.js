34de4b9ed456343f4e3f661b3d85a9d6b5a482e7
Simon Brown <simonandon@live.com.au>
2024-12-09 11:32:00 +0800

    Sorting wordArray by length

--- algorithm/data/words.js
+++ algorithm/data/words.js
@@ -2,6 +2,10 @@ import fs from 'node:fs';
 import wordListPath from 'word-list';
 
 // generates an array of english words
-const wordArray = fs.readFileSync(wordListPath, 'utf8').toUpperCase().split('\n');
+const wordArray = fs
+	.readFileSync(wordListPath, 'utf8')
+	.toUpperCase()
+	.split('\n')
+	.sort((a, b) => a.length -b.length);
 
 export default wordArray;
