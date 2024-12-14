28c38694b6e3f9e83103fd0103ed35842cf52b21
Simon Brown <simonandon@live.com.au>
2024-12-14 14:14:48 +0800

    Trying to read cache before generating new perms

--- algorithm/permAlgorithm.js
+++ algorithm/permAlgorithm.js
@@ -1,6 +1,8 @@
 import wordArray from './data/words.js';
 import { createEmptyState, testState } from './data/emptyState.js';
 import getWordScore from './data/score.js';
+import getPermutaions from './data/permutations.js';
+import getPermsFromCache from './data/getPermsFromCache.js';
 
 /**
  *
@@ -69,7 +71,8 @@ const getValidWords = (hand, state) => {
 	];
 
 	// get permutations for hand
-	const permutations = getPermutaions(hand);
+	// try cache first otherwise generate new perms
+	const permutations = getPermsFromCache().perms || getPermutaions(hand);
 
 	const validWords = [];
 	// loop through vertical anchors and find valid permutations of letter
@@ -450,114 +453,6 @@ const getValidWords = (hand, state) => {
 	return [filteredValidWords, validWords.length];
 };
 
-const getPermutaions = (hand) => {
-	console.log('permutations:');
-	const results = [];
-
-	// helper function to generate subsets of hand
-	const generateSubsets = (subset, start) => {
-		// generate permutations for each subset
-		if (subset.length > 0) {
-			results.push(...permute(subset));
-		}
-		for (let i = start; i < hand.length; i++) {
-			generateSubsets([...subset, hand[i]], i + 1);
-		}
-	};
-
-	// helper function to generate permutations of the subset
-	const permute = (arr) => {
-		// if (arr.length === 1) return [arr];
-		if (arr.length === 1) {
-			const isJoker = arr[0] === 'j';
-			return [
-				{
-					permutation: arr,
-					jokers: isJoker ? 1 : 0,
-					jokerIndices: isJoker ? [0] : [],
-				},
-			];
-		}
-
-		const permutations = [];
-		for (let i = 0; i < arr.length; i++) {
-			const current = arr[i];
-			// slice out current letter
-			const remaining = arr.slice(0, i).concat(arr.slice(i + 1));
-			for (const perm of permute(remaining)) {
-				const p = [current, ...perm.permutation];
-
-				// how many jokers are in this permutation
-				let jokers = 0;
-				const jokerIndices = [];
-
-				for (let k = 0; k < p.length; k++) {
-					const letter = p[k];
-					if (letter === 'j') {
-						jokerIndices.push(k);
-						jokers++;
-					}
-				}
-
-				// add all perms to permutations
-				permutations.push({
-					permutation: p,
-					jokers: jokers,
-					jokerIndices: jokerIndices,
-				});
-			}
-		}
-
-		// filter out perms that don't exist as components of words
-		const filteredPermutations = [];
-		for (const p of permutations) {
-			const pString = p.permutation.join('');
-			if (p.jokers === 0) {
-				p.regExp = new RegExp(pString);
-				for (const word of wordArray) {
-					if (word.includes(pString)) {
-						filteredPermutations.push(p);
-						break;
-					}
-				}
-			} else {
-				// if there are jokers in permutation
-				// dynamically generate regex to test with
-				let jokerRegExpString = '';
-				for (const letter of p.permutation) {
-					if (letter !== 'j') {
-						jokerRegExpString += letter;
-					} else {
-						jokerRegExpString += '[A-Z]';
-					}
-				}
-				// store regex in permutation object
-				// const jokerRegExp = new RegExp(jokerRegExpString);
-				p.regExp = new RegExp(jokerRegExpString);
-				for (const word of wordArray) {
-					if (p.regExp.test(word)) {
-						filteredPermutations.push(p);
-						console.log(p.permutation);
-						break;
-					}
-				}
-			}
-		}
-		return filteredPermutations;
-	};
-
-	generateSubsets([], 0);
-	console.log('permutations length:', results.length);
-	return results;
-};
-
 const hand = ['A', 'B', 'N', 'j', 'S', 'E', 'T'];
 const validWords = getValidWords(hand, testState);
 console.log(validWords);
-console.log(validWords.length);
-// for (let i = 0; i < 10; i++) console.info(validWords[i]);
-// console.log(validWords.length);
-
-// const perms = getPermutaions(hand);
-
-// console.log(perms.length);
