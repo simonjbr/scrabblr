b62539ea3f9e16d3ed02ff668efcc4585d0a7c46
Simon Brown <simonandon@live.com.au>
2025-01-08 09:35:34 +0800

    Now storing the dimensions of detections in the detection objects both in pixel size and size relative to game grid squares. These a very useful ratios. Also found that cloud vison can sometimes combine placed letters and bonus squares into one detection. This will be a real pain to fix but I think its doable

--- OCR/util/parseDetects.js
+++ OCR/util/parseDetects.js
@@ -21,18 +21,21 @@ const parseDetects = (detections, dimensions) => {
 	for (let i = 1; i < detections.length; i++) {
 		const d = detections[i];
 
-		const dim = {};
+		// dimensions of the detection
+		d.dim = { pixel: {} };
 
-		dim.x = d.boundingPoly.vertices[1].x - d.boundingPoly.vertices[0].x;
-		dim.y = d.boundingPoly.vertices[3].y - d.boundingPoly.vertices[0].y;
+		d.dim.pixel.x =
+			d.boundingPoly.vertices[1].x - d.boundingPoly.vertices[3].x;
+		d.dim.pixel.y =
+			d.boundingPoly.vertices[3].y - d.boundingPoly.vertices[1].y;
 
 		if (
 			d.boundingPoly.vertices[0].y > 300 &&
 			dimensions.height - d.boundingPoly.vertices[0].y > 300 &&
 			/^[A-Z]+$/.test(d.description)
 		) {
-			maxX = Math.max(dim.x, maxX);
-			maxY = Math.max(dim.y, maxY);
+			maxX = Math.max(d.dim.pixel.x, maxX);
+			maxY = Math.max(d.dim.pixel.y, maxY);
 			// console.log(dim);
 			if (!topLeftIndex) {
 				topLeftIndex = i;
@@ -57,8 +60,8 @@ const parseDetects = (detections, dimensions) => {
 
 	// size of each grid square
 	const boxSize =
-		(bottomRight.boundingPoly.vertices[0].x -
-			topLeft.boundingPoly.vertices[0].x) /
+		(bottomRight.boundingPoly.vertices[0].y -
+			topLeft.boundingPoly.vertices[0].y) /
 		14;
 	console.log('boxSize:', boxSize);
 
@@ -67,7 +70,11 @@ const parseDetects = (detections, dimensions) => {
 
 	// filter out all non game grid detections
 	// and sort left to right and top to bottom
-	const sortedAndFiltered = sortAndFilterDetects(detections, dimensions);
+	const sortedAndFiltered = sortAndFilterDetects(
+		detections,
+		dimensions,
+		boxSize
+	);
 
 	return {
 		boxSize: boxSize,
--- OCR/util/sortAndFilterDetects.js
+++ OCR/util/sortAndFilterDetects.js
@@ -4,12 +4,13 @@ import fs from 'node:fs';
  *
  * @param {{description: String, boundingPoly: {vertices: {x: number, y: number}[]}}[]} detections OCR detections
  * @param {{height: number, width: number}} dimensions dimensions of the screenshot
+ * @param {number} boxSize size of each square in the game grid
  * @returns {{description: String, boundingPoly: {vertices: {x: number, y: number}[]}}[]}
  */
 
 // filter out all non game grid detections
 // and sort left to right and top to bottom
-const sortAndFilterDetects = (detections, dimensions) => {
+const sortAndFilterDetects = (detections, dimensions, boxSize) => {
 	const sortedAndFiltered = detections
 		// may need a more robust pattern in the future
 		.filter(
@@ -33,7 +34,7 @@ const sortAndFilterDetects = (detections, dimensions) => {
 				? a.boundingPoly.vertices[0].y - b.boundingPoly.vertices[0].y
 				: 0
 		)
-		// Adding a center vertex to account for differently sized text detects
+		// adding a center vertex to account for differently sized text detects
 		.map((d) => {
 			const centerVertex = {};
 			centerVertex.x =
@@ -45,6 +46,27 @@ const sortAndFilterDetects = (detections, dimensions) => {
 				(d.boundingPoly.vertices[2].y - d.boundingPoly.vertices[0].y) /
 					2;
 			d.boundingPoly.vertices.push(centerVertex);
+
+			// calculate dimensions as a proportion of box size
+			d.dim.relativeToBox = {};
+			d.dim.relativeToBox.x = d.dim.pixel.x / boxSize;
+			d.dim.relativeToBox.y = d.dim.pixel.y / boxSize;
+
+			// if detection is 2 chars long and still fits in one box we know its a bonus square
+			d.isBonus = false;
+			if (d.description.length === 2 && d.dim.relativeToBox.x < 1) {
+				d.isBonus = true;
+			}
+
+			// cloud vision can sometimes combine placed letters and bonus squares into one detection
+			// i'll need to determine which portions of the string are tiles/bonuses and
+			// split the detect into multiple detects with updated vertices and dimensions
+			if (
+				d.description.length > 2 &&
+				d.dim.relativeToBox.x < d.description.length - 1
+			) {
+				d.weHaveAProblem = true;
+			}
 
 			return d;
 		});
