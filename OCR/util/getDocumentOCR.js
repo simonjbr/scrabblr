77befd5bc1a7662e301e1af44acd96e888bbd711
Simon Brown <simonandon@live.com.au>
2025-01-12 11:52:00 +0800

    created getDocumentOCR, which uses client.documentTextDetect instead of client.textDetect, for testing

---
+++ OCR/util/getDocumentOCR.js
@@ -0,0 +1,53 @@
+import 'dotenv/config';
+import vision from '@google-cloud/vision';
+import fs from 'node:fs';
+
+const CREDENTIALS = JSON.parse(process.env.CLOUD_VISION_CRED || {});
+
+const CONFIG = {
+	credentials: {
+		private_key: CREDENTIALS.private_key,
+		client_email: CREDENTIALS.client_email,
+	},
+};
+
+const client = new vision.ImageAnnotatorClient(CONFIG);
+
+/**
+ *
+ * @param {string} imagePath path to screenshot
+ * @returns {{description: String, boundingPoly: {vertices: {x: number, y: number}[]}}[]}
+ */
+
+const getDocumentOCR = async (imagePath) => {
+	try {
+		const response = await client.documentTextDetection(imagePath);
+		const result = response[0];
+		const detections = result.textAnnotations;
+
+		fs.writeFileSync(
+			'./OCR/results/response.json',
+			JSON.stringify({ response }),
+			'utf8'
+		);
+
+		fs.writeFileSync(
+			'./OCR/results/result.json',
+			JSON.stringify({ result }),
+			'utf8'
+		);
+
+		fs.writeFileSync(
+			'./OCR/results/detections.json',
+			JSON.stringify({ detections }),
+			'utf8'
+		);
+
+		return detections;
+	} catch (error) {
+		console.error(error.message);
+		return error;
+	}
+};
+
+export default getDocumentOCR;
