76b5e798bc788bd382b66ca0815c1ca8d3e29b38
Simon Brown <simonandon@live.com.au>
2025-01-13 11:43:01 +0800

    Improved the filterDuplicates function to deal with an array of detected coordinates

--- OCR/util/filterDuplicates.js
+++ OCR/util/filterDuplicates.js
@@ -12,12 +12,24 @@ const filterDuplicates = (detections) => {
 			coords: { minX, minY },
 		} = d;
 		if (seen.has(description)) {
-			return (
-				Math.abs(seen.get(description).minX - minX) > 20 &&
-				Math.abs(seen.get(description).minY - minY) > 20
+			if (d.description === 'O') {
+				console.log('');
+			}
+			const matches = seen.get(description);
+			const isDuplicate = matches.some(
+				(existing) =>
+					Math.abs(existing.minX - minX) < 20 &&
+					Math.abs(existing.minY - minY) < 20
 			);
+
+			if (isDuplicate) {
+				return false;
+			} else {
+				matches.push({ minX, minY });
+				return true;
+			}
 		} else {
-			seen.set(description, { minX, minY });
+			seen.set(description, [{ minX, minY }]);
 			return true;
 		}
 	});
