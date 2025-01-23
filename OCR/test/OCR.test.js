3208d77ead647d03777e8a2a3a52c184d352fff4
Simon Brown <simonandon@live.com.au>
2025-01-23 16:59:45 +0800

    Added a new test for 10_1440.jpg

--- OCR/test/OCR.test.js
+++ OCR/test/OCR.test.js
@@ -9,6 +9,106 @@ import { describe, expect, jest, test } from '@jest/globals';
 // jest.useFakeTimers();
 
 describe('OCR and board state creation', () => {
+	test('10_1440.jpg', async () => {
+		const actualBoardState = [
+			[
+				'',
+				'',
+				'',
+				'U',
+				'T',
+				'I',
+				'L',
+				'I',
+				'D',
+				'O',
+				'R',
+				'',
+				'H',
+				'O',
+				'W',
+			],
+			['', '', '', '', 'O', '', '', '', '', 'W', 'E', '', 'O', '', ''],
+			['', '', '', '', 'M', '', '', '', 'J', '', 'G', '', 'G', '', ''],
+			['', '', '', 'F', 'A', 'N', '', 'P', 'A', '', 'U', '', 'G', '', ''],
+			[
+				'R',
+				'E',
+				'D',
+				'A',
+				'T',
+				'E',
+				'',
+				'E',
+				'X',
+				'T',
+				'R',
+				'A',
+				'I',
+				'T',
+				'S',
+			],
+			['O', '', '', 'D', 'O', 'E', '', 'A', 'Y', '', '', '', 'N', '', 'O'],
+			['Q', '', '', 'E', '', '', '', 'Z', '', '', '', '', '', '', 'L'],
+			['U', '', '', 'S', 'P', 'I', 'N', 'E', '', '', '', '', '', '', 'A'],
+			['E', '', '', '', 'A', 'T', 'E', 'S', '', '', '', '', '', '', 'R'],
+			['', '', '', '', 'V', '', '', '', 'A', '', '', '', 'K', '', ''],
+			['', '', '', '', 'E', '', '', '', 'N', 'U', 'B', '', 'I', 'C', 'E'],
+			[
+				'',
+				'',
+				'',
+				'B',
+				'R',
+				'A',
+				'V',
+				'E',
+				'D',
+				'',
+				'A',
+				'',
+				'L',
+				'',
+				'',
+			],
+			['', '', 'N', 'E', 'S', 'T', '', '', '', '', 'N', '', 'L', '', ''],
+			['', '', '', '', '', '', '', 'F', 'I', 'L', 'T', 'H', 'S', '', ''],
+			['', '', '', '', '', '', '', '', '', '', 'Y', '', '', '', ''],
+		];
+
+		// expect.assertions(1);
+
+		const {
+			dimensions: { width, height },
+			detailedWords,
+		} = await getDocumentOCR('./OCR/assets/10_1440.jpg');
+
+		const dimensions = getDimensions(width, height);
+
+		const parsedDetects = parseDetects(dimensions, detailedWords);
+
+		const boardState = createBoardState(parsedDetects);
+
+		expect(boardState.length).toBe(actualBoardState.length);
+
+		let count = 0;
+		for (let i = 0; i < boardState.length; i++) {
+			expect(`Row ${i}: ${boardState[i].length}`).toBe(
+				`Row ${i}: ${actualBoardState[i].length}`
+			);
+			expect(`Row ${i}: ${JSON.stringify(boardState[i])}`).toBe(
+				`Row ${i}: ${JSON.stringify(actualBoardState[i])}`
+			);
+			count += boardState[i].length;
+		}
+
+		expect(count).toBe(15 * 15);
+
+		expect(JSON.stringify(boardState)).toBe(
+			JSON.stringify(actualBoardState)
+		);
+	}, 40000);
+
 	test('09_1440.jpg', async () => {
 		const actualBoardState = [
 			[
@@ -450,6 +550,90 @@ describe('OCR and board state creation', () => {
 			dimensions: { width, height },
 			detailedWords,
 		} = await getDocumentOCR('./OCR/assets/08_1080.jpg');
+
+		const dimensions = getDimensions(width, height);
+
+		const parsedDetects = parseDetects(dimensions, detailedWords);
+
+		const boardState = createBoardState(parsedDetects);
+
+		expect(boardState.length).toBe(actualBoardState.length);
+
+		let count = 0;
+		for (let i = 0; i < boardState.length; i++) {
+			expect(`Row ${i}: ${boardState[i].length}`).toBe(
+				`Row ${i}: ${actualBoardState[i].length}`
+			);
+			expect(`Row ${i}: ${JSON.stringify(boardState[i])}`).toBe(
+				`Row ${i}: ${JSON.stringify(actualBoardState[i])}`
+			);
+			count += boardState[i].length;
+		}
+
+		expect(count).toBe(15 * 15);
+
+		expect(JSON.stringify(boardState)).toBe(
+			JSON.stringify(actualBoardState)
+		);
+	}, 40000);
+
+	test('07_joker_1080.jpg', async () => {
+		const actualBoardState = [
+			[
+				'',
+				'',
+				'',
+				'U',
+				'T',
+				'I',
+				'L',
+				'I',
+				'D',
+				'O',
+				'R',
+				'',
+				'',
+				'',
+				'',
+			],
+			['', '', '', '', '', '', '', '', '', 'W', 'E', '', '', '', ''],
+			['', '', '', '', '', '', '', '', '', '', 'G', '', '', '', ''],
+			['', '', '', '', '', '', '', 'P', 'A', '', 'U', '', '', '', ''],
+			[
+				'',
+				'',
+				'',
+				'',
+				'',
+				'',
+				'',
+				'E',
+				'X',
+				'T',
+				'R',
+				'A',
+				'I',
+				'T',
+				'S',
+			],
+			['', '', '', '', '', '', '', 'A', '', '', '', '', '', '', 'O'],
+			['', '', '', '', '', '', '', 'Z', '', '', '', '', '', '', 'L'],
+			['', '', '', 'S', 'P', 'I', 'N', 'E', '', '', '', '', '', '', 'A'],
+			['', '', '', '', 'A', '', '', '', '', '', '', '', '', '', 'R'],
+			['', '', '', '', 'V', '', '', '', '', '', '', '', '', '', ''],
+			['', '', '', '', 'E', '', '', '', '', '', '', '', '', '', ''],
+			['', '', '', 'B', 'R', 'A', 'V', 'E', '', '', '', '', '', '', ''],
+			['', '', 'N', 'E', 'S', 'T', '', '', '', '', '', '', '', '', ''],
+			['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+			['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+		];
+
+		// expect.assertions(1);
+
+		const {
+			dimensions: { width, height },
+			detailedWords,
+		} = await getDocumentOCR('./OCR/assets/07_joker_1080.jpg');
 
 		const dimensions = getDimensions(width, height);
 
