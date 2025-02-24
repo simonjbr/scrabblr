e62f4684fd5b443fedeca1d71e41fce5fed83369
Simon Brown <simonandon@live.com.au>
2025-02-24 11:11:40 +0800

    Installed tailwindcdd

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import { ImageUploadForm } from '../../components/ImageUploadForm';
 export const Home = () => {
 	return (
 		<div>
+			<h1 className="text-3xl font-bold underline">Tailwind in action</h1>
 			<ImageUploadForm />
 		</div>
 	);
