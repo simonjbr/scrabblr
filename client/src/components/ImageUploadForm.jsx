2c9cbebb6edd8bec6e5c8e3372141e27c63f9a51
Simon Brown <simonandon@live.com.au>
2025-02-28 11:19:33 +0800

    Improved error handling in handleSubmit and used functional state update for setBoard

--- client/src/components/ImageUploadForm.jsx
+++ client/src/components/ImageUploadForm.jsx
@@ -1,21 +1,35 @@
 import { useEffect, useState } from 'react';
 import { useGetOCR } from '../hooks/useGetOCR';
+import { useBoardContext } from '../context/BoardContext';
 
 export const ImageUploadForm = () => {
 	const [image, setImage] = useState(null);
-	const [result, setResult] = useState({
-		boardState: [],
-		hand: [],
-	});
 
 	const { getOCR, loading, error } = useGetOCR();
+	const { board, setBoard } = useBoardContext();
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 
-		const newResult = await getOCR(image);
+		if (!image) {
+			console.warn('No image!');
+			return;
+		}
 
-		setResult(newResult);
+		try {
+			const newResult = await getOCR(image);
+			console.log('OCR result:', newResult);
+
+			if (!newResult || !newResult.boardState) {
+				console.error('Failed to get OCR!');
+				return;
+			}
+			console.log('New board state:', newResult.boardState);
+			setBoard((prevBoard) => [...newResult.boardState]);
+			console.log(newResult.boardState);
+		} catch (error) {
+			console.error(error);
+		}
 	};
 
 	const handleFileChange = (e) => {
@@ -28,9 +42,12 @@ export const ImageUploadForm = () => {
 	};
 
 	useEffect(() => {
-		if (error) console.error(error);
-		if (!loading) console.log(result);
-	}, [result, loading, error]);
+		if (board.length > 0) console.log('Board updated:', board);
+	}, [board]);
+
+	useEffect(() => {
+		if (error) console.error('OCR error:', error);
+	}, [error]);
 
 	return (
 		<div>
