c083a50c3bfb9219ea0bd41cd18ea72a26092762
Simon Brown <simonandon@live.com.au>
2025-03-05 11:44:22 +0800

    Added a basic post route to serve the 10 best moves for a provided hand and boardState

--- server/controller/api/index.js
+++ server/controller/api/index.js
@@ -3,7 +3,9 @@ import express from 'express';
 const router = express.Router();
 
 import cloudVisionRoutes from './cloudVisionRoutes.js';
+import bestMovesRoutes from './bestMovesRoutes.js';
 
 router.use('/cloudVision', cloudVisionRoutes);
+router.use('/bestMoves', bestMovesRoutes);
 
 export default router;
