ddcf98bb585c8a88764c47ddc28d98522e3a8adf
Simon Brown <simonandon@live.com.au>
2025-03-05 12:28:26 +0800

    Created hook useGetBestMoves to provide getBestMoves which will send the post request to /api/bestMoves

---
+++ client/src/hooks/useGetBestMoves.js
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react';
+
+export const useGetBestMoves = () => {
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
+	const [controller, setController] = useState(null);
+
+	const getBestMoves = async (hand, board) => {
+		if (controller) controller.abort();
+
+		const newController = new AbortController();
+		setController(newController);
+
+		setLoading(true);
+		setError(null);
+		try {
+			const response = await fetch('/api/bestMoves', {
+				signal: newController.signal,
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					hand,
+					board,
+				}),
+			});
+
+			if (!response.ok) throw new Error('Failed to fetch best moves');
+
+			return response.json();
+		} catch (error) {
+			if (error.name !== 'AbortError') {
+				console.error(error);
+				setError(error);
+			}
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	useEffect(() => {
+		return () => {
+			if (controller) controller.abort();
+		};
+	}, [controller]);
+
+	return { getBestMoves, loading, error };
+};
