7e9b6eb0ce181d81183cc7630f1eb8c0af94028d
Simon Brown <simonandon@live.com.au>
2025-03-18 12:51:15 +0800

    Added wordfeud board/tile colors to the tailwind theme

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,11 @@
 import daisyui from 'daisyui';
+
+/** @type {import('tailwindcss').Config} */
 export default {
-	//...
+	content: [
+		'./index.html',
+		'./src/**/*.{js,ts,jsx,tsx}',
+		'./src/components/**/*.{js,jsx,ts,tsx}',
+	],
 	plugins: [daisyui],
 };
