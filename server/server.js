52bb3ff82f79159424ba6df43fe307c42e7b18c5
Simon Brown <simonandon@live.com.au>
2025-04-02 13:33:24 +0800

    Set up server to serve static files from dist when in production

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import routes from './controller/index.js';
+import path from 'path';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,6 +9,17 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
+
+const __dirname = path.resolve();
+
+// if in production serve client side bundle
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '/client/dist')));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname, '/client/dist/index.html'));
+	});
+}
 
 app.listen(PORT, () => {
 	console.log(`app is listening at http://localhost:${PORT}`);
